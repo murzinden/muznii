4da16c026d3788753311899daa3d1ce1969ba66a
murzinden <80094162+murzinden@users.noreply.github.com>
2023-10-18 22:39:54 +0400

    deploy BASEURL

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
     return (
         <>
-            <Router basename="/muznii">
+            <Router basename={import.meta.env.BASE_URL}>
                 <Header/>
                 <Routes>
                     <Route path="/" element={<Project/>}/>
